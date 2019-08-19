02e88d8f170d386616e97c05241c7e0dcd2231ef
ZAbrham <abrhamzemene16@gmail.com>
2019-08-19 01:43:16 -0400

    Added the files

---
+++ ShellJr/shell_jr.c
@@ -0,0 +1,1 @@
+/* Implement your shell here */
---
+++ Text Manipulation/text_manipulation.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <string.h>
+#include "text_manipulation.h"
+
+int remove_spaces(const char *source, char *result, int *num_spaces_removed) {  
+}
+
+int center(const char *source, int width, char *result) {       
+}
